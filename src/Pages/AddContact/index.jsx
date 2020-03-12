66206d1a5e21f3db11c2df40ff75d045bead5bc3
Anton Reshetov <Reshetov190991@mail.ru>
2020-03-12 12:35:32 +0200

    вынес форму для создания контакта в контейнер

--- src/Pages/AddContact/index.jsx
+++ src/Pages/AddContact/index.jsx
@@ -1,24 +1,12 @@
 import React from "react";
-import CreateContactForm from "../../Container/CreateContactForm";
-import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
+import CreateContactForm from "../../Container/CreateContactForm/index"
 import "./style.css";
 
 const AddContact = () => {
-  const [toggle, setToggle] = React.useState(false);
   return (
     <Container maxWidth="sm">
-      <h1>Добавить контакт</h1>
-      <div className="button"> 
-        <Button
-          variant="contained"
-          className="button"
-          onClick={() => setToggle(!toggle)}
-        >
-          Добавить контакт
-        </Button>
-      </div>
-      {toggle && <CreateContactForm />}
+      <CreateContactForm />
     </Container>
   );
 };
