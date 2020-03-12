5f2251430152e9e68235f7a8349c78d219f9091d
Anton Reshetov <Reshetov190991@mail.ru>
2020-03-12 10:54:09 +0200

    Исправил toggle

--- src/Container/CreateContactForm/index.jsx
+++ src/Container/CreateContactForm/index.jsx
@@ -1,24 +1,20 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
 import "./style.css";
-import ContactTable from "../ContactTable";
 
 const CreateContactForm = () => {
-  const [toggle, setToggle] = React.useState(false);
   return (
     <form>
       <TextField id="standard-basic" label="Name" className="input" />
       <TextField id="standard-basic" label="Phone" className="input" />
       <TextField id="standard-basic" label="Email" className="input" />
-      <Button
-        variant="contained"
-        className="button"
-        onClick={() => setToggle(!toggle)}
-      >
-        Добавить
-      </Button>
-      {toggle && <ContactTable />}
+      <Link to="/contacts">
+        <Button variant="contained" className="button">
+          Сохранить
+        </Button>
+      </Link>
     </form>
   );
 };
--- src/Pages/AddContact/index.jsx
+++ src/Pages/AddContact/index.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import CreateContactForm from "../../Container/CreateContactForm";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import "./style.css";
 
 const AddContact = () => {
+  const [toggle, setToggle] = React.useState(false);
   return (
-    <div>
-      <CreateContactForm />
-    </div>
+    <Container maxWidth="sm">
+      <h1>Добавить контакт</h1>
+      <div className="button"> 
+        <Button
+          variant="contained"
+          className="button"
+          onClick={() => setToggle(!toggle)}
+        >
+          Добавить контакт
+        </Button>
+      </div>
+      {toggle && <CreateContactForm />}
+    </Container>
   );
 };
 
