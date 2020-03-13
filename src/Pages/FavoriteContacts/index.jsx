6fb707ea8148e99f43f0bc5062bc520a2b693be3
Anton Reshetov <Reshetov190991@mail.ru>
2020-03-13 12:21:34 +0200

    переиспользовал ContactTable

--- src/Pages/FavoriteContacts/index.jsx
+++ src/Pages/FavoriteContacts/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import FavoriteTable from "../../Container/FavoriteTable";
+import ContactTable from "../../Container/ContactTable";
 
 
 const FavoriteContacts = () => {
   return (
     <div>
-      <FavoriteTable />
+      <ContactTable/>
     </div>
   );
 };
--- src/Pages/Main/index.jsx
+++ src/Pages/Main/index.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import Container from "@material-ui/core/Container";
 import "./style.css";
 import ContactTable from "../../Container/ContactTable";
 
 const Main = () => {
   return (
-    <Container maxWidth="sm">
+    <div>
       <ContactTable />
-    </Container>
+    </div>
   );
 };
 
