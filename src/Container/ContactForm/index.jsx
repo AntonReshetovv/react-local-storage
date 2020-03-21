c30e8c18fade9cc378d06b513d69c2e36ecba860
Anton Reshetov <Reshetov190991@mail.ru>
2020-03-21 09:12:47 +0200

    Добавил  useState в addContact и прокинул через пропсы в таблицу setы

--- src/Container/ContactForm/index.jsx
+++ src/Container/ContactForm/index.jsx
@@ -7,11 +7,29 @@ import "./style.css";
 const ContactForm = props => {
   return (
     <form>
-      <TextField id="standard-basic" label="Name" className="input" />
-      <TextField id="standard-basic" label="Phone" className="input" />
-      <TextField id="standard-basic" label="Email" className="input" />
+      <TextField
+        id="standard-basic"
+        label="Name"
+        className="input"
+        value={props.name}
+        onChange={props.setName}
+      />
+      <TextField
+        id="standard-basic"
+        label="Phone"
+        className="input"
+        value={props.phone}
+        onChange={props.setPhone}
+      />
+      <TextField
+        id="standard-basic"
+        label="Email"
+        className="input"
+        value={props.email}
+        onChange={props.setEmail}
+      />
       <Link to="/" className="link">
-        <Button variant="contained" className="button">
+        <Button variant="contained" className="button" onClick={props.click}>
           {props.isContactNew ? "Добавить" : "Обновить"}
         </Button>
       </Link>
--- src/Pages/AddContact/index.jsx
+++ src/Pages/AddContact/index.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "@material-ui/core/Container";
 import ContactForm from "../../Container/ContactForm";
 import "./style.css";
 
 const AddContact = () => {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+
   return (
     <Container maxWidth="sm">
       <h2>Создание контакта</h2>
-      <ContactForm isContactNew={true}/>
+      <ContactForm
+        isContactNew={true}
+        setName={e => setName(e.target.value)}
+        setPhone={e => setPhone(e.target.value)}
+        setEmail={e => setEmail(e.target.value)}
+        name={name}
+        phone={phone}
+        email={email}
+        click = {()=>console.log('я кликнул')}
+      />
     </Container>
   );
 };
